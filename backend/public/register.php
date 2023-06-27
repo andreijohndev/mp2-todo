6b037251580decae801e2816a74a3aecdc13991b
andreijohndev <andreijohn.dev@gmail.com>
2023-06-27 20:35:07 +0800

    Removed old code and fixed README

--- backend/public/register.php
+++ backend/public/register.php
@@ -18,30 +18,11 @@ $postData = file_get_contents("php://input");
 $userController = new UserController();
 $result = $userController->RegisterUser($postData);
 
+// Response data schema
+// {
+//     "status": 200,
+//     "message": "Output message"
+// }
 http_response_code($result["status"]);
 echo json_encode($result);
-/*$data = json_decode(file_get_contents("php://input"));
-$username = $data->username;
-$password = $data->password;
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-$query = "INSERT INTO Users (username, password) VALUES (?, ?)";
-
-$stmt = $database->conn->prepare($query);
-$stmt->bind_param("ss", $username, $hashed_password);
-
-if ($stmt->execute()) {
-    // TODO: Maybe add email verification?
-    http_response_code(200);
-    echo json_encode([
-        "status" => 200,
-        "message" => "User was successfully registered."
-    ]);
-} else {
-    http_response_code(400);
-    echo json_encode([
-        "status" => 400,
-        "message" => "Unexpected error while registering the user."
-    ]);
-}*/
 ?>
